eea61d9d06a3251f26d9376c5300f713ab1f3c34
bharath1005 <bharatharun.k@outlook.com>
2018-11-08 15:50:44 +0530

    Added someCodes

---
+++ src/org/scanner/Test.java
@@ -0,0 +1,16 @@
+package org.scanner;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Employee Details");
+		
+		}
+	 private void empID() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
